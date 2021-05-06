27ab1bba409ed09aadce85208501058952abd0c1
Swongliver <81005472+Swongliver@users.noreply.github.com>
2021-05-06 18:06:37 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hello, I am Scott");
+console.log("I don't know how to add or multiply in JavaScript")
+var b="15"
+console.log(b);
+var c="5"
+console.log(c);
+var a="The addition of 15 and 5 is "
+console.log(a);
+var add="20";
+console.log(add);
+
+var b="50"
+console.log(b);
+var c="2"
+console.log(c);
+var a="The addition of 50 and 2 is "
+console.log(a);
+var multiply="100";
+console.log(multiply);
